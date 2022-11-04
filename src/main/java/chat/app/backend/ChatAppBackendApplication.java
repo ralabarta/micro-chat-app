21c51118f7210a42533a4ccfb5f8f9b440e79c15
Rubiel Alejandro González Labarta <74202468+ralabarta@users.noreply.github.com>
2022-11-04 08:29:56 -0400

    Add files via upload

---
+++ src/main/java/chat/app/backend/ChatAppBackendApplication.java
@@ -0,0 +1,13 @@
+package chat.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppBackendApplication.class, args);
+    }
+
+}
